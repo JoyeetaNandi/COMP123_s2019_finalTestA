a26ea89f2039ef7d0ee16e557b7d8ecacf8cacfa
Joyeeta <biplob@STUDY-PC>
2019-08-09 09:07:01 -0400

    Base project

---
+++ COMP123_s2019_finalTestA/Objects/Power.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Student Name: 
+ * Student Id:
+ * Description: This is the Power Class
+ */
+namespace COMP123_s2019_finalTestA.Objects
+{
+    class Power
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
